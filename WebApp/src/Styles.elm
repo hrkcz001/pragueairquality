f22928c944f1e388b22d040d5c649be26f593419
morozan1 <morozan1@fit.cvut.cz>
2023-06-25 11:06:36 +0200

    basic map implementation

---
+++ WebApp/src/Styles.elm
@@ -0,0 +1,91 @@
+
+module Styles exposing
+    ( addDebt
+    , content
+    , dropdown
+    , input
+    , label
+    , person
+    , personDetail
+    , personLink
+    , personList
+    , textarea
+    )
+
+import Html exposing (Attribute)
+import Html.Attributes as Attributes
+
+
+content : List (Attribute msg)
+content =
+    [ Attributes.style "display" "flex" ]
+
+
+personList : List (Attribute msg)
+personList =
+    [ Attributes.style "flex-basis" "15%"
+    , Attributes.style "margin" "1rem"
+    , Attributes.style "padding-right" "1rem"
+    , Attributes.style "border-right" "1px solid #000"
+    ]
+
+
+person : List (Attribute msg)
+person =
+    [ Attributes.style "padding" ".25rem 0" ]
+
+
+personLink : List (Attribute msg)
+personLink =
+    [ Attributes.style "color" "blue"
+    , Attributes.style "text-decoration" "underline"
+    , Attributes.style "cursor" "pointer"
+    ]
+
+
+personDetail : List (Attribute msg)
+personDetail =
+    [ Attributes.style "flex-basis" "65%"
+    , Attributes.style "position" "relative"
+    ]
+
+
+addDebt : List (Attribute msg)
+addDebt =
+    [ Attributes.style "flex-basis" "20%"
+    , Attributes.style "margin" "1rem"
+    , Attributes.style "padding-left" "1rem"
+    , Attributes.style "border-left" "1px solid #000"
+    ]
+
+
+label : List (Attribute msg)
+label =
+    [ Attributes.style "display" "block"
+    , Attributes.style "font-weight" "bold"
+    , Attributes.style "margin-bottom" ".5rem"
+    ]
+
+
+dropdown : List (Attribute msg)
+dropdown =
+    [ Attributes.style "margin-bottom" "1rem"
+    ]
+
+
+input : List (Attribute msg)
+input =
+    [ Attributes.style "width" "100%"
+    , Attributes.style "padding" "0.25rem"
+    , Attributes.style "margin-bottom" "1rem"
+    ]
+
+
+textarea : List (Attribute msg)
+textarea =
+    [ Attributes.style "width" "100%"
+    , Attributes.style "padding" "0.25rem"
+    , Attributes.style "margin-bottom" "1rem"
+    , Attributes.style "resize" "vertical"
+    , Attributes.style "min-height" "5rem"
+    ]
